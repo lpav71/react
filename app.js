008c4a2b8a2527f7f70983bf7ccf0092800cd97e
DESKTOP-M9JJK92\admin <l>
2024-09-20 22:59:02 +0300

    Сделан дубль на функциях главного файла App.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Импорт CSS Bootstrap
 import 'bootstrap'; // Импорт JS Bootstrap
@@ -6,28 +6,24 @@ import 'bootstrap'; // Импорт JS Bootstrap
 import ComponentOne from './src/ComponentOne';
 import ComponentTwo from './src/ComponentTwo';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { welcome: "Кнопка" };
-    }
+const App = () => {
+    const welcome = "Кнопка";
 
-    render() {
-        return (
-            <div>
-                <h1 className='mb-3' style={{ margin: '20px auto', width: '642px' }}>
-                    Простой проект с React и Webpack
-                </h1>
-                <hr />
-                <ComponentOne />
-                <ComponentTwo />
-            </div>
-        );
-    }
-}
+    return (
+        <div>
+            <h1 className='mb-3' style={{ margin: '20px auto', width: '642px' }}>
+                Простой проект с React и Webpack
+            </h1>
+            <hr />
+            <ComponentOne />
+            <ComponentTwo />
+        </div>
+    );
+};
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 root.render(<App />);
+
 
 
